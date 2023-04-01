3d594a3b4e5f0b77ab2b414cd9c76917e852155e
arsen056 <vurdihanov@gmail.com>
2023-04-01 14:17:46 +0300

    feat: add eslint rules

--- src/shared/sharedHeader/SharedHeader.tsx
+++ src/shared/sharedHeader/SharedHeader.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+
 import style from './SharedHeader.module.scss'
 
-
-
 const SharedHeader = () => {
-    return (
-        <div className={style.headerBlock}>
-  <div className={style.title}>Inctagram</div>
-        </div>
-    );
+  return (
+    <div className={style.headerBlock}>
+      <div className={style.title}>Inctagram</div>
+    </div>
+  );
 };
 
 export default SharedHeader;
