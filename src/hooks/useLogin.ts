fdf6400bf1d5b281c0242f0aaed1f080be6e9872
arsen056 <vurdihanov@gmail.com>
2023-04-02 14:21:18 +0300

    feat: validation field login

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,30 @@
+import { useForm } from 'react-hook-form';
+
+export type LoginFormFields = {
+  email: string;
+  password: string;
+};
+
+const emailPattern = {
+  value: new RegExp('^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$', 'ig'),
+  message: 'Enter a valid email address.',
+};
+
+export const useLogin = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormFields>();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log('submitting...');
+  };
+
+  const emailRules = { required: 'Check your email.', pattern: emailPattern }
+  const passwordRules = { required: 'You must enter your password.' }
+
+  return { onSubmit, handleSubmit, register, emailRules, errors, passwordRules }
+};
