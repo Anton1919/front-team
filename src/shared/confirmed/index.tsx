da87f8f8fa92b613e39f7dd1a917a08bc5410440
arsen056 <vurdihanov@gmail.com>
2023-04-05 01:58:50 +0300

    feat: add confirmed and expired components

--- src/shared/confirmed/index.tsx
+++ src/shared/confirmed/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import Image from 'next/image';
+
+import { useRouter } from 'next/router';
+
+import { Button } from '@/shared/button/Button';
+import img from '@/assets/images/confirmed.png';
+import { Container } from '@/shared/container';
+
+import s from './Confirmed.module.scss'
+
 export const Confirmed = () => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h2>Confirmed</h2>
-    </div>
+    <Container className={s.container}>
+      <h2>Congratulations!</h2>
+      <p>Your email has been confirmed</p>
+      <Button button_name={'Sign In'} button_handler={() => router.push('/login')} />
+      <Image className={s.img} src={img} alt={'Congratulations'}/>
+    </Container>
   );
 };
