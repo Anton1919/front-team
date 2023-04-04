7ad836a8e43f9db7ad1ff8914912648915da2dc0
kotpavlik <kotpavlik88@gmail.com>
2023-04-04 17:38:51 +0300

    fix: remove textarea in login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,13 +3,9 @@ import React from 'react';
 import Head from 'next/head';
 
 import { Login } from '@/features/login/ui';
-import { Textarea } from '@/shared/textarea/Textarea';
 
 const LoginPage = () => {
 
-  const onChangeThis = (e: any) => {
-    console.log(e.currentTarget.value)
-  }
   return (
     <>
       <Head>
@@ -17,8 +13,6 @@ const LoginPage = () => {
       </Head>
 
       <Login />
-      <Textarea name={'fake name'} sx={{ width: 500, height: 200 }}
-        label={'Label for textarea'} disabled={false} handleTextareaChange={onChangeThis} />
     </>
   )
 };
