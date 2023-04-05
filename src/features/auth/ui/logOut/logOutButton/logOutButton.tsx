fc52c0b83620ce56cbddf644093df31c069cc690
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2023-04-05 10:39:20 +0300

    feat(logOut): modal shared component

--- src/features/auth/ui/logOut/logOutButton/logOutButton.tsx
+++ src/features/auth/ui/logOut/logOutButton/logOutButton.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import Image from 'next/image';
 
 import LogOutIcon from '../../../../../assets/icons/logOut.svg'
 
 import s from './LogOutButton.module.scss'
-const LogOutButton = () => {
-  const onClickHandler=()=>{alert('Hi!!!')}
+
+type LogOutButtonPropsType = {
+    callBack: (value: boolean) => void
+}
+const LogOutButton: FC<LogOutButtonPropsType> = ({ callBack }) => {
+  const onClickHandler = () => {
+    callBack(true)
+  }
   return (
     <div onClick={onClickHandler} className={s.logoutBlock}>
       <Image className={s.icon} src={LogOutIcon} alt={'icon'}/>
