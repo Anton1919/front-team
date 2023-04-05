6e40fd605f03630109f624f51a7a4d863d38e9c7
Anton1919 <bf3kenzo@mail.ru>
2023-04-06 00:02:28 +0300

    add Select-box component

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Head from 'next/head';
 
 import { Login } from '@/features/auth/ui/login';
-import SelectBox from '@/shared/select';
 
 const LoginPage = () => {
   return (
     <>
-      <SelectBox></SelectBox>
-
       <Head>
         <title>Login</title>
       </Head>
-
       <Login />
 
     </>
