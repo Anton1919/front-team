dd81b973ee982274caf23eb625861283f01a9b23
arsen056 <vurdihanov@gmail.com>
2023-04-06 03:11:52 +0300

    feat: add login, without refresh tokens

---
+++ src/features/auth/hooks/useMeQuery.ts
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { AuthAPI } from '@/features/auth/api';
+
+import { selectSetEmail, selectSetIsAuth, useAuthStore } from '@/features/auth/store';
+
+export const useMeQuery = () => {
+  const setEmail = useAuthStore(selectSetEmail)
+  const setIsAuth = useAuthStore(selectSetIsAuth)
+  return useQuery({
+    queryFn: AuthAPI.me,
+    queryKey: ['me'],
+    retry: false,
+    refetchIntervalInBackground: false,
+    refetchInterval: false,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    onSuccess: (data) => {
+      setEmail(data.email)
+      setIsAuth(true)
+    },
+    onError: () => {
+      setIsAuth(false)
+    }
+  });
+};
