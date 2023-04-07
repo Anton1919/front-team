8bc8f2f472e6666c8850c45e2e8aacc589a69547
Kiri1111 <92_medved@mail.ru>
2023-04-07 10:52:30 +0300

    fix: styles create profile page

--- src/features/auth/ui/create-account/index.tsx
+++ src/features/auth/ui/create-account/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import s from '@/features/auth/ui/create-account/CreateAccount.module.scss';
 import { BaseInput } from '@/shared/input';
 import { Textarea } from '@/shared/textarea/Textarea';
+import { Button } from '@/shared/button/Button';
 
 const CreateAccount = () => {
   return (
@@ -14,48 +15,50 @@ const CreateAccount = () => {
       <div>
 
         <form className={s.form}>
-          <BaseInput
-            id={'userName'}
-            name={'userName'}
-            label={'UserName'}
-            // register={register}
-            // rules={emailRules}
-            // error={errors.email?.message}
-          />
-          <BaseInput
-            id={'name'}
-            name={'name'}
-            label={'Name'}
-            // register={register}
-            // rules={emailRules}
-            // error={errors.email?.message}
-          />
-          <BaseInput
-            id={'surName'}
-            name={'surName'}
-            label={'SurName'}
-            // register={register}
-            // rules={emailRules}
-            // error={errors.email?.message}
-          />
-          <input type="date"/>
-          <BaseInput
-            id={'city'}
-            name={'city'}
-            label={'City'}
-            // register={register}
-            // rules={emailRules}
-            // error={errors.email?.message}
-          />
-          <div>About Me</div>
-          <Textarea name={'About Me'} handleTextareaChange={() => {
-          }}/>
+          <div>ADDDD PPPHOOTTOO</div>
+          <div className={s.inputs}>
+            <BaseInput
+              id={'userName'}
+              name={'userName'}
+              label={'UserName'}
+              // register={register}
+              // rules={emailRules}
+              // error={errors.email?.message}
+            />
+            <BaseInput
+              id={'name'}
+              name={'name'}
+              label={'Name'}
+              // register={register}
+              // rules={emailRules}
+              // error={errors.email?.message}
+            />
+            <BaseInput
+              id={'surName'}
+              name={'surName'}
+              label={'SurName'}
+              // register={register}
+              // rules={emailRules}
+              // error={errors.email?.message}
+            />
+            <input type="date"/>
+            <BaseInput
+              id={'city'}
+              name={'city'}
+              label={'City'}
+              // register={register}
+              // rules={emailRules}
+              // error={errors.email?.message}
+            />
+            <div>About Me</div>
+            <Textarea name={'About Me'} handleTextareaChange={() => {
+            }}/>
+            <Button button_name={'Create Account'}/>
+          </div>
         </form>
-
       </div>
-      <div>ADDDD PPPHOOTTOO</div>
-    </div>
 
+    </div>
   );
 };
 
