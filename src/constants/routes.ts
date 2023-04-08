da33e385283eddee19ed40ed23cbf0c8b9c07dd9
arsen056 <vurdihanov@gmail.com>
2023-04-08 09:41:12 +0300

    refactor: add routes constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,9 @@
+export const publicPaths = {
+  login: '/login',
+  register: '/registration',
+  confirmMail: '/confirm-email',
+  passRecovery: '/password-recovery',
+  forgotPass: '/forgot-password',
+  mailSent: '/confirm-message',
+  confirmed: '/confirm-message/confirmed'
+} as const
