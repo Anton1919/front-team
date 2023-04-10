b6cded68c5d3db3df2672b01970919d9e1005868
arsen056 <vurdihanov@gmail.com>
2023-04-10 11:38:05 +0300

    refactor: textarea, props react hook form

--- src/shared/textarea/Textarea.tsx
+++ src/shared/textarea/Textarea.tsx
@@ -1,64 +1,36 @@
+import { ComponentProps } from 'react';
+import classNames from 'classnames';
+import { FieldValues, Path, RegisterOptions, UseFormRegister } from 'react-hook-form';
+
 import s from './Textarea.module.scss';
 
-export interface TextareaTypes {
-  name: string
-  id?: string
-  placeholder?: string
-  cols?: number
-  rows?: number
-  sx?: {
-    width?: number
-    height?: number
-  }
+export type TextareaTypes<TFormValues extends FieldValues> = {
   error?: string
-  disabled?: boolean
-  label?: string
-  value?: string
-  // eslint-disable-next-line no-unused-vars
-  handleTextareaChange: (event?: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  label?: string,
+  name: Path<TFormValues>
+  rules?: RegisterOptions
+  register?: UseFormRegister<TFormValues>
+} & ComponentProps<'textarea'>
+
+export const Textarea = <TFormValues extends Record<string, unknown>>
+  ({ className,  label, disabled, error, register, name, rules, ...rest } : TextareaTypes<TFormValues>) => {
+
+  let stylesForTextarea = s.my_textarea_style
+  if (error) stylesForTextarea = s.my_textarea_error
+  if (disabled) stylesForTextarea = s.my_disabled_textarea
+
+  return (
+    <>
+      {label &&
+        <div className={disabled ? s.label_text_disabled : s.label_text}>
+          {label}
+        </div>}
+      <textarea className={classNames(stylesForTextarea, className)} {...(register && register(name, rules))} {...rest} />
+      {disabled ?
+        null
+        : <div className={s.error_text}>
+          {error && error}
+        </div>}
+    </>
+  );
 }
-
-export const Textarea =
-  ({
-    name,
-    id,
-    placeholder,
-    cols,
-    rows,
-    sx,
-    error,
-    disabled = false,
-    label,
-    value,
-    handleTextareaChange
-  }: TextareaTypes) => {
-
-    let stylesForTextarea = s.my_textarea_style
-    if (error) stylesForTextarea = s.my_textarea_error
-    if (disabled) stylesForTextarea = s.my_disabled_textarea
-
-    return (
-      <>
-        {label &&
-          <div className={disabled ? s.label_text_disabled : s.label_text}>
-            {label}
-          </div>}
-        <textarea
-          name={name}
-          id={id ? id : ''}
-          placeholder={placeholder}
-          cols={cols}
-          rows={rows}
-          style={sx}
-          className={stylesForTextarea}
-          value={value}
-          onChange={(e) => handleTextareaChange(e)}
-          disabled={disabled}/>
-        {disabled ?
-          <></>
-          : <div className={s.error_text}>
-            {error && error}
-          </div>}
-      </>
-    );
-  }
