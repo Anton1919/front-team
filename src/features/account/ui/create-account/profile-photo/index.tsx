70ed0214d70715c21c60f384c85d5c21f035b6d0
Anton1919 <bf3kenzo@mail.ru>
2023-04-10 21:09:27 +0300

    build Profile Photo component

--- src/features/account/ui/create-account/profile-photo/index.tsx
+++ src/features/account/ui/create-account/profile-photo/index.tsx
@@ -22,8 +22,8 @@ const ProfilePhoto = () => {
   };
   const onPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
     setToggleModal(true);
-    if (e) {
-      setImageFile(e?.target?.files[0]);
+    if (e.target) {
+      setImageFile(e.target?.files[0]);
     }
   };
   const onSavePhotoHandler = () => {
