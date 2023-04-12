e68e12d3f82d0141aee43ce86a3fc9844f6b5ca3
Kiri1111 <92_medved@mail.ru>
2023-04-12 21:55:54 +0300

    feat: redirect in create account

--- src/pages/create-account/index.tsx
+++ src/pages/create-account/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
 
+import { useRouter } from 'next/router';
+
 import CreateAccount from '@/features/account/ui/create-account';
+import { selectIsAuth, useAuthStore } from '@/features/auth/store';
+import { PATHS } from '@/constants/routes';
 
 const AccountCreate = () => {
+
+  const { push } = useRouter()
+
+  const isAuth=useAuthStore(selectIsAuth)
+
+  if(!isAuth){
+    push(PATHS.PUBLIC.LOGIN)
+    return
+  }
+
   return (
     <>
       <Head>
