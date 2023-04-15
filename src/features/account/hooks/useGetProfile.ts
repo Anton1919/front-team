1c91759a4ebcddebf6edc907203bd97a4b712205
arsen056 <vurdihanov@gmail.com>
2023-04-15 17:06:36 +0300

    feat: show date to changeSettings

--- src/features/account/hooks/useGetProfile.ts
+++ src/features/account/hooks/useGetProfile.ts
@@ -3,12 +3,14 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { AccountAPI } from '@/features/account/api';
 import { selectSetUser, useCreateAccountStore } from '@/features/account/store';
 import { ProfileType } from '@/features/account/types';
+import { isoDate } from '@/common/utils/isoDate';
 
 export const useGetProfile = ():  UseQueryResult<ProfileType> => {
   const setUser = useCreateAccountStore(selectSetUser)
   return useQuery({
     queryFn: AccountAPI.getProfile,
     queryKey: ['getProfile'],
+    select: (data: ProfileType) => ({ ...data, birthday: isoDate(data.birthday as string) }),
     retry: false,
     refetchInterval: false,
     refetchOnReconnect: false,
