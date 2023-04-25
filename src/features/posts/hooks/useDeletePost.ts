253d4bd952da4f765e302fa0cf374c334cef6f3a
Kiri1111 <92_medved@mail.ru>
2023-04-25 20:16:53 +0300

    feat : Delete post api, hook

--- src/features/posts/hooks/useDeletePost.ts
+++ src/features/posts/hooks/useDeletePost.ts
@@ -1,11 +1,9 @@
 import { useMutation } from '@tanstack/react-query'
 
-import { QUERY_KEY } from '@/common/constants/queryKeys'
 import { PostsAPI } from '@/features/posts/api'
 
 export const useDeletePost = (): any => {
   return useMutation({
-    queryKey: [QUERY_KEY.DELETE_POSTS],
     mutationFn: PostsAPI.deletePost,
   })
 }
